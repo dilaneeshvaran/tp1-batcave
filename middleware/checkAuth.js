974aa7e72d2e394feca11afed35d42bc1843af2e
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-05-19 21:53:10 +0200

    implement rate limit for login

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const db = require("../db");
+const { isBlocked, recordFailure, recordSuccess } = require("./loginLimiter");
 
 const checkAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -17,13 +18,36 @@ const checkAuth = async (req, res, next) => {
     .toString()
     .split(":");
 
+  // verify if user is blocked temporarily
+  const { blocked, remainingMs } = isBlocked(username);
+  if (blocked) {
+    const remainingSec = Math.ceil(remainingMs / 1000);
+    return res
+      .status(429)
+      .send(
+        `<script>alert('trop de tentatives... réessayez dans${remainingSec} secondes'); window.location.href = '/';</script>`,
+      );
+  }
+
   const user = db
     .prepare("SELECT * FROM users WHERE username = ?")
     .get(username);
   if (user && (await bcrypt.compare(password, user.password_hash))) {
+    recordSuccess(username);
     req.user = user;
     next();
   } else {
+    recordFailure(username);
+    const { blocked: nowBlocked, remainingMs: newRemainingMs } =
+      isBlocked(username);
+    if (nowBlocked) {
+      const remainingSec = Math.ceil(newRemainingMs / 1000);
+      return res
+        .status(429)
+        .send(
+          `<script>alert('trop de tentative detectée, compte bloqué pendant ${remainingSec} seconde'); window.location.href = '/';</script>`,
+        );
+    }
     return res
       .status(401)
       .send(
