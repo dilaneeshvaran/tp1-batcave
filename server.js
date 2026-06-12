83875cb0b93b9bddcf209bb7f3baee134217c675
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-12 18:31:26 +0200

    setup express session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const authRouter = require("./routes/auth");
 const batcomputerRouter = require("./routes/batcomputer");
 const adminRouter = require("./routes/admin");
@@ -7,6 +8,20 @@ const adminRouter = require("./routes/admin");
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(
+  session({
+    name: "bat_identity",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 1800000,
+    },
+  })
+);
 
 // Mount entity routers
 app.use("/", authRouter);
