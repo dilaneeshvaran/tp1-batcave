031f90c69b602948ea17f65de470061c278af5f6
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-07-16 12:11:07 +0200

    fix authCheck sending user  to login even when refresh is valid

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -4,10 +4,24 @@ const isAuthenticated = (req, res, next) => {
   if (verifyAndRefreshTokens(req, res)) {
     return next();
   }
+
   res.status(401);
-  return res.send(
-    "<script>alert('authentification requise'); window.location.href = '/auth/login';</script>"
-  );
+  return res.send(`
+<script>
+fetch("/api/auth/refresh", { method: "POST" })
+  .then((r) => {
+    if (r.ok) {
+      console.log("token rafraîchi, rechargement de la page");
+      location.reload();
+    } else {
+      location.href = "/auth/login";
+    }
+  })
+  .catch(() => {
+    location.href = "/auth/login";
+  });
+</script>
+`);
 };
 
 isAuthenticated.isAuthenticated = isAuthenticated;
