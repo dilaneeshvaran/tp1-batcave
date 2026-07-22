478c7428bb6a665e18b51abf319bd48f3c3d3dca
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-07-22 17:57:42 +0200

    auth and rersource servers

--- server.js
+++ server.js
@@ -39,3 +39,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
 });
+
+require("./auth-server");
+require("./resource-server");
